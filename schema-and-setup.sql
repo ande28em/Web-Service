83f5dae3acf0ec6f0f14fe3d357dd156cd9b10c8
Brantley Cervarich <cervarbn@dukes.jmu.edu>
2021-11-02 19:49:29 -0400

    maybe

--- schema-and-setup.sql
+++ schema-and-setup.sql
@@ -7,12 +7,14 @@ CREATE DATABASE social CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 CREATE USER social_user@localhost IDENTIFIED WITH mysql_native_password BY 'social';
 GRANT ALL PRIVILEGES ON social.* TO social_user@localhost;
 
-use social;
+USE social
+GO
 
-DROP TABLE IF EXISTS guest;
+DROP TABLE IF EXISTS guest
 
 CREATE TABLE guest (
   id SERIAL PRIMARY KEY,
   firstname text,
   lastname text
-);
+)
+GO
