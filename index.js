74d1d5e08221821efa984ae79d8e7eea3a72e743
Eric Anderson <ande28em@dukes.jmu.edu>
2021-12-09 16:47:39 -0500

    Improvement to cross origin POST

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ service.use((request, response, next) => {
   response.set('Access-Control-Allow-Origin', '*');
   next();
 });
+service.options('*', (request, response) => {
+  response.set('Access-Control-Allow-Headers', 'Content-Type');
+  response.set('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  response.sendStatus(200);
+});
+
 
 //-----------------------------------------------------
 
